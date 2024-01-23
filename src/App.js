d8cbabb2606885077824c07a3ab393ed3914a696
Mancung <60146927+MancungXYZ@users.noreply.github.com>
2024-01-23 12:45:50 +0700

    markdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import { Marked, marked } from 'marked';
 function App() {
+  const [text, setText] = useState(`
+  # Sample Markdown Header Level
+
+## Sample Header Level 2
+
+### Link
+
+Here's a link to [Codepen](https://codepen.io).
+
+### Code Block
+
+1.  Open the file.
+2.  Find the following code block on line 21:
+
+        <html>
+          <head>
+            <title>Test</title>
+          </head>
+
+3.  Update the title to match the name of your website.
+
+### Inline Code
+
+I think you should use an \`<addr>\` element here instead.
+
+### List
+
+- First item
+- Second item
+
+### Blockquote
+
+> Dorothy followed her through many of the beautiful rooms in her castle.
+
+### Image
+
+![Markdown Logo](https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Markdown-mark.svg/208px-Markdown-mark.svg.png "Markdown Logo")
+
+### Bold Text
+
+I just love **bold text**.
+`)
+
+  marked.options({
+    breaks: true
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id="editor" onChange={(event) => {setText(event.target.value)}} value={text}></textarea>
+        <div id="preview" dangerouslySetInnerHTML={{__html: marked(text)}}></div>
     </div>
   );
 }
